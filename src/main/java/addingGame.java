b2a82e78c3deafa0b0999268143fcef824fe5065
oliver bevan <oliverbevan@outlook.com>
2020-07-28 21:37:43 +0100

    adding game first commit

---
+++ src/main/java/addingGame.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class addingGame {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        String input;
+        int result = 0;
+
+        int number1 = random.nextInt(100);
+        int number2 = random.nextInt(100);
+
+        System.out.println("Add Up \n" + number1 + "\n" + number2);
+
+        int answer = number1 + number2;
+
+        StringBuilder sb = new StringBuilder("Enter value: ");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter value: ");
+        input = scanner.nextLine();
+        result = Integer.parseInt(input) +result;
+
+        if (result == answer){
+            System.out.println("Good job!!");
+        }
+
+    }
+}
